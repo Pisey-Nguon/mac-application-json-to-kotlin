3bad6ff5b35b3d73f0b7a16dbc227d8104ddd038
Pisey Nguon <pisey.nguon@igtech.asia>
2026-05-28 14:37:17 +0700

    Initial Commit

---
+++ json to kotlin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  json to kotlin
+//
+//  Created by Pisey Nguon on 28/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ json to kotlin/json_to_kotlinApp.swift
@@ -0,0 +1,17 @@
+//
+//  json_to_kotlinApp.swift
+//  json to kotlin
+//
+//  Created by Pisey Nguon on 28/5/26.
+//
+
+import SwiftUI
+
+@main
+struct json_to_kotlinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
